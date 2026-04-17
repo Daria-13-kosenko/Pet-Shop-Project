98a12874975655ceef9a396dc623fe46c3ae404e
Daria_Kosenko <dasha.milkaaa@gmail.com>
2026-04-17 15:33:38 +0200

    add fixed

--- Pet-Shop-Backend/routes/order.js
+++ Pet-Shop-Backend/routes/order.js
@@ -1,18 +1,17 @@
-const { request } = require('express');
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.json({ message: 'order route works' })
+})
 
-router.get('/send', (req, res) =>{
-    res.json({});
-
+router.get('/send', (req, res) => {
+  res.json({})
 })
 
 router.post('/send', (req, res) => {
-    
-    res.json({status: 'OK', message: 'request processed'})
+  res.json({ status: 'OK', message: 'request processed' })
 })
 
-
-module.exports = router;
+module.exports = router
